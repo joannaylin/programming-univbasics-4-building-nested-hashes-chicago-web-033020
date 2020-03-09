80df663654ee2105213cf307838c46b09a5425f2
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-09 18:51:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,10 @@ def monopoly_with_second_tier
   # complexity.
   hash = {
     :railroads => {
-      :pieces => 4
+      :pieces => 4,
+      :rent_in_dollars => {
+        
+      }
     }
   }
 end
